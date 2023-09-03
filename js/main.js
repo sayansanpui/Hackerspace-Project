fe114235f7f3d0bf2f4661f5a2a6d1b828f9a000
sayansanpui <sayansanpuicoding@gmail.com>
2023-09-03 14:25:49 +0530

    Commit 6

--- js/main.js
+++ js/main.js
@@ -1,26 +1,18 @@
-
-// active navbar
-// Script for navigation bar
 const bar = document.getElementById('bar');
 const close = document.getElementById('close');
 const nav = document.getElementById('navbar');
 
 if (bar) {
     bar.addEventListener('click', () => {
-        nav.classList.add('active');
+        nav.classList.add('active2');
+        bar.classList.add('active3');
     })
 }
 
 if (close) {
     close.addEventListener('click', () => {
-        nav.classList.remove('active');
+        nav.classList.remove('active2');
+        bar.classList.remove('active3');
     })
 }
-
-
-// nav hide
-
-
-// counter design
-
 
