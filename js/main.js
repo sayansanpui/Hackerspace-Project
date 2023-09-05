d1c8b41e43ecb41505b5d77b1853581ed9090825
sayansanpui <sayansanpuicoding@gmail.com>
2023-09-05 09:13:36 +0530

    Update V-0.1

--- js/main.js
+++ js/main.js
@@ -31,3 +31,18 @@ dlmode.onclick = function() {
     }
 }
 
+
+
+const observer = new IntersectionObserver((entries) =>{
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+        else {
+            entry.target.classList.remove('show');
+        }
+    })
+})
+
+const hiddenElement = document.querySelectorAll('.hidden');
+hiddenElement.forEach((el) => observer.observe(el));
